dc4179318c9bf2a4f1f0bdf19a60b5e672b5a771
Matthew Young <radicalmatt@fastmail.com>
2020-11-19 20:33:27 -0600

    gemini-insert-header-auto test coverage increase
    Additional test macro facility that allows the creation of a temporary
    buffer with an active region instead of just a point.

--- test/gemini-mode--header-insert-tests.el
+++ test/gemini-mode--header-insert-tests.el
@@ -40,6 +40,91 @@
 	       "### Level <POINT> Heading"
 	       (expect (gemini-header-level-at-point) :to-equal 3))))
 
+(describe "gemini-insert-header-auto"
+	  ;; Level Detection
+	  (it "Inserts a level 1 header in an empty buffer"
+	      (with-gemini-buffer
+	       ""
+	       (gemini-insert-header-auto)
+	       (expect (buffer-string) :to-equal "# ")))
+
+	  ;; Inserting with text at point 
+	  (it "Takes a line of text containing point and translates it into a header"
+	      (with-gemini-buffer
+	       "This is a <POINT> line with the point"
+	       (gemini-insert-header-auto)
+	       (expect (buffer-string)
+		       :to-equal "# This is a line with the point")))
+	  (it "Does not modify existing header at point if no level change occurs"
+	      (with-gemini-buffer
+	       "# This is a <POINT> header with the point"
+	       (gemini-insert-header-auto)
+	       (expect (buffer-string)
+		       :to-equal "# This is a header with the point")))
+	  (it "Does not modify an existing header at point"
+	      (with-gemini-buffer
+	       "## Preceeding Header\n\n# This is a <POINT> header with the point"
+	       (gemini-insert-header-auto)
+	       (expect (buffer-string)
+		       :to-equal "## Preceeding Header\n\n# This is a header with the point")))
+
+	  ;; Insertion with text region
+	  (it "Uses active region text is used as header content"
+	      (with-gemini-buffer-region
+	       "This is a <REGION-START>line of text<REGION-END>"
+	       (gemini-insert-header-auto)
+	       (expect (buffer-string)
+		       :to-equal "This is a\n\n# line of text")))
+	  (it "Collapses weird weird whitespace in the active region into a header"
+	      (with-gemini-buffer-region
+	       "<REGION-START> This\t\nis   \n a\n line of \n\ttext<REGION-END>"
+	       (gemini-insert-header-auto)
+	       (expect (buffer-string)
+		       :to-equal "# This is a line of text")))
+	  (it "Inserts new header using active region in existing header at same level"
+	      (with-gemini-buffer-region
+	       "## This is a <REGION-START>line of text<REGION-END>"
+	       (expect (buffer-string)
+		       :to-equal "## This is a \n## line of text")))
+
+	  ;; Spacing
+	  (it "Will not insert a blank line preceeding the header
+	  when inserting at beginning of the buffer"
+	      (with-gemini-buffer
+	       "<POINT>\n\n"
+	       (gemini-insert-header-auto)
+	       (expect (buffer-string) :to-equal "# \n\n")))
+	  (it "Will not insert a blank line following the header when inserting at the end of bufer"
+	      (with-gemini-buffer
+	       "\n\n<POINT>"
+	       (gemini-insert-header-auto)
+	       (expect (buffer-string) :to-equal "\n\n# ")))
+	  (it "Inserts appropriate space between a new header and the preceeding line"
+	      (with-gemini-buffer
+	       "This is a line that preceeds the point\n<POINT>"
+	       (gemini-insert-header-auto)
+	       (expect (buffer-string)
+		       :to-equal "This is a line that preceeds the point\n\n# ")))
+	  (it "Inserts appropriate space between a new header and the following line"
+	      (with-gemini-buffer
+	       "<POINT>\nThis is a line that follows the point"
+	       (gemini-insert-header-auto)
+	       (expect (buffer-string)
+		       :to-equal "# \n\nThis is a line that follows the point")))
+	  (it "Inserts appropriate space around a new header"
+	      (with-gemini-buffer
+	       "Preceeding Line\n<POINT>\nFollowing Line"
+	       (gemini-insert-header-auto)
+	       (expect (buffer-string)
+		       :to-equal "Preceeding Line\n\n# \n\nFollowing Line")))
+	  (it "Does not insert space when the new header is not crowded on either side"
+	      (with-gemini-buffer
+	       "Before text\n\nHeader <POINT> Line\n\nAfter Text"
+	       (gemini-insert-header-auto)
+	       (expect (buffer-string)
+		       :to-equal "Before text\n\n# Header Line\n\nAfter Text")))
+)
+
 (provide 'gemini-mode--header-insert-tests)
 
 ;;; gemini-mode--header-insert-tests.el ends here
--- test/utils/test-gemini-mode--macro-utils.el
+++ test/utils/test-gemini-mode--macro-utils.el
@@ -1,4 +1,4 @@
-;;; test-gemini-mode--macro-utils.el
+;;; test-gemini-mode--macro-utils.el -*- lexical-binding: t; -*-
 
 ;;; Commentary:
 
@@ -17,23 +17,64 @@
 
 Use this to set up test cases that rely on point positioning.")
 
+(defvar region-regex "\\(?1:<REGION-START>\\)\\(?2:.*?\\)\\(?2:<REGION-END>\\)")
+(defvar region-start "<REGION-START>")
+(defvar region-end "REGION-END>")
+
 (defmacro with-gemini-buffer (contents &rest body)
   "Executes BODY in a temporary gemini-mode buffer with CONTENTS
-  as the buffer content.
+  as the buffer content. Will set the point to a particular
+  position when a sentinel is provided in CONTENTS.
 
-CONTENTS is a string that contains the optional sentinel value
-`<POINT>` indicating the desired point position in the temporary
-buffer. If no `<POINT>` string is present, the point is placed at
-the end of the buffer"
+CONTENTS is a string that will be used as the buffer contents. 
+
+CONTENTS can contain the optional sentinel value `<POINT>`
+indicating the desired point position in the temporary buffer. If
+no `<POINT>` string is present, the point is placed at the end of
+the buffer."
   `(with-temp-buffer
      (delay-mode-hooks (gemini-mode))
      (insert ,contents)
      (goto-char (point-min))
-     ;; search for sentinel, 'noerror moves to limit of search and
-     ;; returns nil when not found
+       
+     
+     ;; If no region is found, search for point sentinel	    
      (when (search-forward point-sentinel nil 'noerror)
        (delete-char (- (length point-sentinel))))
      ,@body))
+
+(defmacro with-gemini-buffer-region (contents &rest body)
+  "Executes BODY in a temporary gemini-mode buffer with CONTENTS
+  as the buffer content. Will set an active region if the region
+  delimiters are provided in CONTENTS.
+
+CONTENTS is a string that will be used as the buffer contents. 
+
+CONTENTS may contain a <REGION-START> and <REGION-END> delimiters
+that will see the text inside of the two delimeters used as the
+active region."
+  `(with-temp-buffer
+     (delay-mode-hooks (gemini-mode))
+     (insert ,contents)
+     (goto-char (point-min))
+     
+     (when (search-forward region-regex nil 'noerror)
+       (let ((begin (match-beginning 0))
+	     (end (match-end 0)))
+	 ;; Okay, here we take the point to the end of the entire match
+	 ;; body, then remove the region-end delimeter. We go to the
+	 ;; start, then remove the region-start delimeter. The point
+	 ;; should now be at the beginning of the second capture group,
+	 ;; so we engage `set-mark-command' and move the point forward
+	 ;; through the second capture group.
+	 ;;
+	 ;; woof.
+	 (point end)
+	 (delete-char (- (length region-end)))
+	 (point begin)
+	 (delete-char (length region-start))
+	 (set-mark-command)
+	 (forward-char (length (match-string-no-properties 2)))))))
 
 		       
 (provide 'test-gemini-mode--macro-utils)
